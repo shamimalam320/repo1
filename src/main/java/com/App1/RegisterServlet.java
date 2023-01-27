11a06f02db3e96735b02dd6a143f6d13475847b7
AVWAPW744 <AVWAPW744@DESKTOP-JFSJMO6.persistent.co.in>
2023-01-27 16:08:53 +0530

    All commit

---
+++ src/main/java/com/App1/RegisterServlet.java
@@ -0,0 +1,37 @@
+package com.App1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //get PrintWriter
+		PrintWriter pw = resp.getWriter();
+		//set resp type
+		resp.setContentType("text/html");
+		
+		//read the form data
+		String name = req.getParameter("name");
+		String designation = req.getParameter("designation");
+		
+		//print the data
+		pw.println("Name: "+name+"<br>");
+		pw.println("Designation: "+name);
+		
+		//close the stream
+		pw.close();
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		doGet(req, resp);
+	}
+
+}

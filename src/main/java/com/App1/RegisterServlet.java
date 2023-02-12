506af58bc53aac30f2bff5116792f9f0d6ce1108
shamim <shamimalam320@gmail.com>
2023-02-12 22:38:09 +0530

    added comment1

--- src/main/java/com/App1/RegisterServlet.java
+++ src/main/java/com/App1/RegisterServlet.java
@@ -1,4 +1,3 @@
-//Testing1
 package com.App1;
 
 import java.io.IOException;
@@ -8,7 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+//comment1
 public class RegisterServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {

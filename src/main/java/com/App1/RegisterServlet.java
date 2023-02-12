6acc479c942fb7fdab7d3133ad59d3d3ff783bc5
shamim <shamimalam320@gmail.com>
2023-02-12 22:56:15 +0530

    removed comment

--- src/main/java/com/App1/RegisterServlet.java
+++ src/main/java/com/App1/RegisterServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//comment1
+
 public class RegisterServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
